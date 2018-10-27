49ee2430960266f6c1731dabbcbd12ec81930997
MaryChCronin <43207966+MaryChCronin@users.noreply.github.com>
2018-10-27 09:39:09 +0100

    esn_data

---
+++ app/src/main/java/com/example/maryc/egtm_calc/esn_data.java
@@ -0,0 +1,97 @@
+package com.example.maryc.egtm_calc;
+
+import java.util.GregorianCalendar;
+
+public class esn_data {
+
+    private String mESN;
+    private String mType;
+    private String mConfiguration;
+    private int mTSN;
+    private int mCSN;
+    private int mEGTM;
+    private GregorianCalendar mRecordDate;
+
+
+    public esn_data() {
+        mESN = "_____________";
+        mType = "_____________";;
+        mConfiguration = "_____________";;
+        mTSN =0;;
+        mCSN = 0;
+        mEGTM = 0;
+        mRecordDate = new GregorianCalendar();
+    }
+
+    public esn_data(String esn, String type, String config, int tsn, int csn, int egtm, GregorianCalendar recordDate){
+    mESN = esn;
+    mType = type;
+    mConfiguration = config;
+    mTSN = tsn;
+    mCSN = csn;
+    mEGTM = egtm;
+    mRecordDate =recordDate;
+}
+
+    public static esn_data getDefaultItem() {
+        return new esn_data("ESN", "CFM56", "5B", 0, 0, 999, new GregorianCalendar());
+    }
+
+    public String getmESN() {
+        return mESN;
+    }
+
+    public void setmESN(String mESN) {
+        this.mESN = mESN;
+    }
+
+    public String getmType() {
+        return mType;
+    }
+
+    public void setmType(String mType) {
+        this.mType = mType;
+    }
+
+    public String getmConfiguration() {
+        return mConfiguration;
+    }
+
+    public void setmConfiguration(String mConfiguration) {
+        this.mConfiguration = mConfiguration;
+    }
+
+    public int getmTSN() {
+        return mTSN;
+    }
+
+    public void setmTSN(int mTSN) {
+        this.mTSN = mTSN;
+    }
+
+    public int getmCSN() {
+        return mCSN;
+    }
+
+    public void setmCSN(int mCSN) {
+        this.mCSN = mCSN;
+    }
+
+    public int getmEGTM() {
+        return mEGTM;
+    }
+
+    public void setmEGTM(int mEGTM) {
+        this.mEGTM = mEGTM;
+    }
+
+    public GregorianCalendar getmRecordDate() {
+        return mRecordDate;
+    }
+
+    public void setmRecordDate(GregorianCalendar mRecordDate) {
+        this.mRecordDate = mRecordDate;
+    }
+}
+
+
