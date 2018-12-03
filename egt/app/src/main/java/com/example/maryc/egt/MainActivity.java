7def3b5cd886ed51ad0cf32dc576aa0abbce338f
AdrianosullivanUL <16230124@studentmail.ul.ie>
2018-12-03 08:26:01 +0000

    compelted Add Engine Record

--- egt/app/src/main/java/com/example/maryc/egt/MainActivity.java
+++ egt/app/src/main/java/com/example/maryc/egt/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.maryc.egt;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,8 +18,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static android.app.PendingIntent.getActivity;
 
@@ -51,8 +59,6 @@ public class MainActivity extends AppCompatActivity {
     private void addResult() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Add Engine Record");
-
-
         View view = getLayoutInflater().inflate(R.layout.dialog_add, null, false);
         builder.setView(view);
 
@@ -93,48 +99,143 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        // temp code to test validation cycle
+        builder.setCancelable(false);
 
         builder.setNegativeButton(android.R.string.cancel, null);
         builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
-
-                String mESN = esn_edittext.getText().toString();
-                String mEngineModel = engine_model_spinner.getSelectedItem().toString();
-                String mEngineDesignation = engine_designation_spinner.getSelectedItem().toString();
-                EngineDesignation engineDesignation = mDBHelper.getEngineDesignation(mEngineModel, mEngineDesignation);
-
-                int mCurrentEGT = Integer.parseInt(current_egt_edittext.getText().toString());
-                int mTimeSinceLastSV = Integer.parseInt(time_since_last_sv_edittext.getText().toString());
-                int mTimeSinceNew = Integer.parseInt(time_since_new_edittext.getText().toString());
-                int mCyclesSinceNew = Integer.parseInt(cycles_since_new_edittext.getText().toString());
-                Date mRecordDate = new Date();
-                String mOperator = ""; //operator_edittext.getText().toString();
-                EngineRecord mEngineRecord;
-                mEngineRecord = new EngineRecord();
-
-                mEngineRecord = new EngineRecord(mESN,
-                        mRecordDate,
-                        mEngineModel,
-                        mOperator,
-                        engineDesignation.getConfiguration(),
-                        engineDesignation.getThrustInK(),
-                        mCurrentEGT,
-                        mTimeSinceLastSV,
-                        mTimeSinceNew,
-                        mCyclesSinceNew, 0);
-
-
-                // todo add method
-
             }
 
         });
 
-        builder.create().show();
-    }
+        //builder.create().show();
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EngineDesignation engineDesignation = null;
+                boolean errorFound = false;
+                // Process eSN
+                String mESN = esn_edittext.getText().toString();
+                if (mESN.length() == 0) {
+                    esn_edittext.requestFocus();
+                    esn_edittext.setError("ESN cannot be empty");
+                    errorFound = true;
+                }
 
+                // Process Engine Model
+                String mEngineModel = engine_model_spinner.getSelectedItem().toString();
+                if (mEngineModel.length() == 0) {
+                    esn_edittext.requestFocus();
+                    esn_edittext.setError("Engine Model cannot be empty");
+                    errorFound = true;
+                }
+
+                // Process Engine Designation
+                String mEngineDesignation = engine_designation_spinner.getSelectedItem().toString();
+                if (mEngineDesignation.length() == 0) {
+                    esn_edittext.requestFocus();
+                    esn_edittext.setError("Engine Designation cannot be empty");
+                    errorFound = true;
+                } else {
+                    engineDesignation = mDBHelper.getEngineDesignation(mEngineModel, mEngineDesignation);
+                }
+
+                // Process Current EGT
+                int mCurrentEGT = 0;
+                boolean mCurrentEGTError = false;
+                try {
+                    mCurrentEGT = Integer.parseInt(current_egt_edittext.getText().toString());
+                } catch (Exception ex) {
+                    current_egt_edittext.requestFocus();
+                    current_egt_edittext.setError("Current EGT cannot be empty");
+                    mCurrentEGTError = true;
+                    errorFound = true;
+                }
+                if (mCurrentEGT <= 0 && mCurrentEGTError == false) {
+                    current_egt_edittext.requestFocus();
+                    current_egt_edittext.setError("Current EGT must be a positive number");
+                    errorFound = true;
+                }
+
+                // Process Time Since Last SV
+                int mTimeSinceLastSV = 0;
+                boolean mTimeSinceLastSVError = false;
+                try {
+                    mTimeSinceLastSV = Integer.parseInt(time_since_last_sv_edittext.getText().toString());
+                } catch (Exception ex) {
+                    time_since_last_sv_edittext.requestFocus();
+                    time_since_last_sv_edittext.setError("Time Since Last SV must be a positive number");
+                    mTimeSinceLastSVError = true;
+                    errorFound = true;
+                }
+                if (mTimeSinceLastSV <= 0 && mTimeSinceLastSVError == false) {
+                    time_since_last_sv_edittext.requestFocus();
+                    time_since_last_sv_edittext.setError("Time Since Last SV must be a positive number");
+                    errorFound = true;
+                }
+
+                // Porcess Time Since New
+                int mTimeSinceNew = 0;
+                boolean mTimeSinceNewError = false;
+                try {
+                    mTimeSinceNew = Integer.parseInt(time_since_new_edittext.getText().toString());
+                } catch (Exception ex) {
+                    time_since_new_edittext.requestFocus();
+                    time_since_new_edittext.setError("Time Since New must be a positive number");
+                    mTimeSinceNewError = true;
+                    errorFound = true;
+                }
+                if (mTimeSinceNew <= 0 && mTimeSinceNewError == false) {
+                    time_since_new_edittext.requestFocus();
+                    time_since_new_edittext.setError("Time Since New must be a positive number");
+                    errorFound = true;
+                }
+
+                // Process Cycles Since New
+                int mCyclesSinceNew = 0;
+                boolean mCyclesSinceNewError = false;
+                try {
+                    mCyclesSinceNew = Integer.parseInt(cycles_since_new_edittext.getText().toString());
+                } catch (Exception ex) {
+                    cycles_since_new_edittext.requestFocus();
+                    cycles_since_new_edittext.setError("Cycles Since New must be a positive number");
+                    mCyclesSinceNewError = true;
+                    errorFound = true;
+                }
+                if (mCyclesSinceNew <= 0 && mCyclesSinceNewError == false) {
+                    cycles_since_new_edittext.requestFocus();
+                    cycles_since_new_edittext.setError("Cycles Since New must be a positive number");
+                    errorFound = true;
+                }
+                if (errorFound == false) {
+                    Date mRecordDate = new Date();
+                    String mOperator = ""; //operator_edittext.getText().toString();
+                    EngineRecord mEngineRecord;
+                    mEngineRecord = new EngineRecord();
+
+                    mEngineRecord = new EngineRecord(mESN,
+                            mRecordDate,
+                            mEngineModel,
+                            mOperator,
+                            engineDesignation.getConfiguration(),
+                            engineDesignation.getThrustInK(),
+                            mCurrentEGT,
+                            mTimeSinceLastSV,
+                            mTimeSinceNew,
+                            mCyclesSinceNew, 0);
+
+                    mDBHelper.addEngineRecord( mEngineRecord);
+                    alertDialog.dismiss();
+                }
+
+
+            }
+        });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -158,3 +259,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
